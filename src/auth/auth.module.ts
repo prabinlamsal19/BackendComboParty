4699642591ead82b3fc817a04a4e1b8e4469847b
prabinlamsal19 <lamsalprabin74@gmail.com>
2023-09-18 12:03:47 +0545

    checkpoint: before endpoints creation:

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,16 @@
 import {Module} from '@nestjs/common'
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
-@Module({})
-export class AuthModule{} 
+@Module({ 
+    controllers: [AuthController], 
+    providers: [AuthService] 
+})
+export class AuthModule{}  
 
 
+//controller needs to call the service(a function from the service) 
+//and controller returns the result to the user 
+//but service ko instance is not created in the controller 
+// we instead use the injectable to provide the instance to the controller
+    
