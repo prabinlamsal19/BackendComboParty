bab3b8ad4a85a62f635748b254686a05abda1d57
prabinlamsal19 <lamsalprabin74@gmail.com>
2023-09-19 12:37:48 +0545

    feat: credentials taken handled for email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service"; 
 import * as argon from 'argon2'; 
 import { AuthDto } from "./dto";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable({})  
 export class AuthService{  
@@ -9,14 +10,32 @@ export class AuthService{
    
    async signup(dto: AuthDto){ 
       const hash = await argon.hash(dto.password); 
-      const user = await this.prisma.user.create({ 
-         data: { 
-            email: dto.email, 
-            hash, 
-         }, 
+      try{ 
 
-      }); 
-      return user; 
+         const user = await this.prisma.user.create({ 
+            data: { 
+               email: dto.email, 
+               hash, 
+            }, 
+            // select: { 
+               //    id: true, 
+               //    email: true, 
+               //    createdAt: true
+               // } 
+               
+            });  
+            delete user.hash;
+            return user; 
+         } catch(error){ 
+            if (error instanceof PrismaClientKnownRequestError) { 
+               if(error.code == "P2002")  { 
+                  throw new ForbiddenException( 
+                     'Credentials taken', 
+                  )
+               }
+            } 
+            throw error
+         }
    } 
 
    login(){ 
