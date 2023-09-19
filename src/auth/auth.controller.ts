c215416e294118eeff719136b99c62ac5f4297c3
prabinlamsal19 <lamsalprabin74@gmail.com>
2023-09-19 11:32:31 +0545

    feat: signup pipes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, ParseIntPipe, Post} from "@nestjs/common";
 import { AuthService } from "./auth.service";
+// import { AuthDto } from "./dto";
 
 //here i am putting a global prefix route 'auth'
 @Controller('auth')    
@@ -7,7 +8,19 @@ export class AuthController{
     constructor(private authService: AuthService){} 
     //now we create endpoints for login and signup  
     @Post('signup')  
-    signup(){ 
+    signup(@Body('email') email : string , 
+            @Body('password', ParseIntPipe) password: string , 
+    ){ 
+        console.log({ 
+            email, typeOfEmail : typeof email , password , typeofPassword: typeof password
+        });   
+        //dto are objects that are used to take in the request 
+        // we do not use the request object. We use the dto instead 
+
+        //normally, here we had to vallidate the input email and name 
+        //but we don't need to do that manually in nest 
+        //we can use the vallidator library 
+
         return this.authService.signup();
     } 
     
