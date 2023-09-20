4fd33c2d75ec5c5b19fe9213fb9c6f33751df72e
prabinlamsal19 <lamsalprabin74@gmail.com>
2023-09-20 14:32:29 +0545

    feat: jwt strategy configured

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import {Module} from '@nestjs/common'
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service'; 
 import {JwtModule} from '@nestjs/jwt';  
+import { JwtStrategy } from './strategy';
 @Module({ 
     imports: [JwtModule.register({})], 
     controllers: [AuthController], 
-    providers: [AuthService] 
+    providers: [AuthService, JwtStrategy],
 })
 export class AuthModule{}  
 
@@ -14,4 +15,8 @@ export class AuthModule{}
 //and controller returns the result to the user 
 //but service ko instance is not created in the controller 
 // we instead use the injectable to provide the instance to the controller
+
+
+//Using the jwt strategy, we can protect some of our routes with jwt strategy
+//i.e. we can only access the pages if we have the correct jwt.. 
     
